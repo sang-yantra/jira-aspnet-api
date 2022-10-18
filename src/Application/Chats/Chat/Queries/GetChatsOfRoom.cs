b6ca302b63f96c0384762fb47c7048e4be36f626
Anup Mahato <anupmahato033@gmail.com>
2022-10-18 21:06:24 +0530

    get chats mediator handler

---
+++ src/Application/Chats/Chat/Queries/GetChatsOfRoom.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chats.Chat.Queries
+{
+    public class GetChatsOfRoom: IRequest<List<ChatDto>>
+    {
+        public Guid RoomId { get; set; }
+    }
+
+    public class GetChatsOfRoomHandler : IRequestHandler<GetChatsOfRoom, List<ChatDto>>
+    {
+        private readonly IJiraDbContext _context; 
+
+        public GetChatsOfRoomHandler(IJiraDbContext context)
+        {
+            _context = context; 
+        }
+        public async Task<List<ChatDto>> Handle(GetChatsOfRoom request, CancellationToken cancellationToken)
+        {
+            var chats = await _context.Chats.Where(x => x.ChatRoomId == request.RoomId)
+                .Select(x => new ChatDto()
+                {
+                    Id = x.Id,
+                    ChatRoomId = x.ChatRoomId,
+                    UserId = x.UserId,
+                    UserName = x.UserName,
+                    Message = x.Message,
+                    CreatedBy = x.CreatedBy,
+                    CreatedDatetime = x.CreatedDatetime,
+                    UpdatedBy = x.UpdatedBy,
+                    UpdatedDatetime = x.UpdatedDatetime,
+                })
+                .OrderByDescending(x => x.CreatedDatetime)
+                .ToListAsync(cancellationToken);
+            return chats;
+        }
+    }
+
+}
---
+++ src/Application/Chats/GlobalUsing.cs
@@ -0,0 +1,4 @@
+﻿global using Jira.Domain.Entities.ChatManagement;
+global using MediatR;
+global using Microsoft.EntityFrameworkCore;
+global using Common.Interfaces;
