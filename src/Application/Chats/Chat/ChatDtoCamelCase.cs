85853036e1768d7cdee50698f9eb40bb478aefef
Anup Mahato <anupmahato033@gmail.com>
2022-11-19 17:17:06 +0530

    Added user avatar in chat socet message

--- src/Application/Chats/Chat/ChatDtoCamelCase.cs
+++ src/Application/Chats/Chat/ChatDtoCamelCase.cs
@@ -6,6 +6,7 @@
         public Guid? chatRoomId { get; set; }
         public Guid? userId { get; set; }
         public string? userName { get; set; }
+        public string avatar { get; set; }
         public string? message { get; set; }
         public DateTime? createdDatetime { get; set; }
         public DateTime? updatedDatetime { get; set; }
